ed57712c654657d925b43ac326812c43a51f46bc
Daniel Majewski <d.majewski.3453@gmail.com>
2020-10-29 15:33:43 -0600

    Create Boot/MVC project

---
+++ BeerHound/src/main/java/com/skilldistillery/beerhound/BeerHoundApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.beerhound;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerHoundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeerHoundApplication.class, args);
+	}
+
+}
---
+++ BeerHound/src/test/java/com/skilldistillery/beerhound/BeerHoundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.beerhound;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerHoundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
