bc5f11d0789e9b22a802ad0a8bfe53d9f71f3892
Tj Laughlin <tlaughl1@gmail.com>
2020-10-30 11:59:06 -0600

    added user entity

--- JPABeerHound/src/main/java/com/skilldistillery/beerhound/entities/User.java
+++ JPABeerHound/src/main/java/com/skilldistillery/beerhound/entities/User.java
@@ -1,5 +1,8 @@
 package com.skilldistillery.beerhound.entities;
 
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -14,31 +17,59 @@ public class User {
 	
 	private String username;
 
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
+	private String password;
+	
+	private int enabled;
+	
+	private String role;
+	
+	
+//	@Column(name = "address_id")
+//	private int addressId;
+	
+	@Column(name = "first_name") 
+	private String firstName;
+	
+	@Column(name = "last_name") 
+	private String lastName;
+	
+	private String email;
+	
+	private String biography;
+	
+	private String image;
+	
+	@Column(name="create_date")	
+	private LocalDateTime createDate;
+	
+	
+	
 	public User() {
 		super();
 	}
 	
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
 	@Override
 	public String toString() {
-		StringBuilder builder = new StringBuilder();
-		builder.append("User [id=").append(id).append(", username=").append(username).append("]");
-		return builder.toString();
+		return "User [id=" + id + ", username=" + username + ", password=" + password + ", enabled=" + enabled
+				+ ", role=" + role + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
+				+ ", biography=" + biography + ", image=" + image + ", createDate=" + createDate + "]";
 	}
 
 
@@ -62,6 +93,96 @@ public class User {
 		if (id != other.id)
 			return false;
 		return true;
+	}
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+	public int getEnabled() {
+		return enabled;
+	}
+
+
+	public void setEnabled(int enabled) {
+		this.enabled = enabled;
+	}
+
+
+	public String getRole() {
+		return role;
+	}
+
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+
+	public String getLastName() {
+		return lastName;
+	}
+
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public String getBiography() {
+		return biography;
+	}
+
+
+	public void setBiography(String biography) {
+		this.biography = biography;
+	}
+
+
+	public String getImage() {
+		return image;
+	}
+
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+
+	public LocalDateTime getCreateDate() {
+		return createDate;
+	}
+
+
+	public void setCreateDate(LocalDateTime createDate) {
+		this.createDate = createDate;
 	}
 	
 
