e508c62ce3561e50778174488590ae90a28dfaf0
Daniel Majewski <d.majewski.3453@gmail.com>
2020-10-30 15:31:41 -0600

    BeerPrice mapping

--- JPABeerHound/src/main/java/com/skilldistillery/beerhound/entities/BeerPrice.java
+++ JPABeerHound/src/main/java/com/skilldistillery/beerhound/entities/BeerPrice.java
@@ -5,6 +5,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class BeerPrice {
@@ -18,6 +20,30 @@ public class BeerPrice {
 	
 	@Column(name = "size_price")
 	private Double sizePrice;
+	
+	@ManyToOne
+	@JoinColumn(name = "beer_id")
+	private Beer beer;
+	
+	@ManyToOne
+	@JoinColumn(name = "bar_id")
+	private Bar bar;
+
+	public Beer getBeer() {
+		return beer;
+	}
+
+	public void setBeer(Beer beer) {
+		this.beer = beer;
+	}
+
+	public Bar getBar() {
+		return bar;
+	}
+
+	public void setBar(Bar bar) {
+		this.bar = bar;
+	}
 
 	public BeerPrice() {
 		super();
