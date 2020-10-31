2ed308b6668176aaf7ae1bb86c8c471a6be4db65
Tj Laughlin <tlaughl1@gmail.com>
2020-10-31 10:59:34 -0600

    created DAO interfaces

---
+++ BeerHound/src/main/java/com/skilldistillery/beerhound/data/BarDAO.java
@@ -0,0 +1,17 @@
+package com.skilldistillery.beerhound.data;
+
+import java.util.List;
+
+import com.skilldistillery.beerhound.entities.Bar;
+
+public interface BarDAO {
+
+	Bar createBar(Bar bar);
+	List<Bar> searchBarByKeyWord(String keyword);
+	Bar updateBar(Bar bar);
+	boolean deleteBar(int id);
+	Bar findBarById(int id);
+	
+	
+	
+}
---
+++ BeerHound/src/main/java/com/skilldistillery/beerhound/data/BeerDAO.java
@@ -0,0 +1,17 @@
+package com.skilldistillery.beerhound.data;
+
+import java.util.List;
+
+import com.skilldistillery.beerhound.entities.Beer;
+
+public interface BeerDAO {
+
+	Beer createBeer(Beer beer);
+	Beer findBeer(int id);
+	List<Beer> findByKeyword(String keyword);
+	Beer updateBeer(Beer beer);
+	boolean deleteBeer(int id);
+	
+
+	
+}
